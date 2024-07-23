544cfc5c88c3bb695dbdb2f98c2bb01c61060bb7
koohyun kwon <rnjsrn98@gmail.com>
2024-07-23 10:53:18 +0000

    v2 생성

---
+++ crawling_v2.py
@@ -0,0 +1,178 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+from subject_crawling_v2 import major_or_designated
+from subject_crawling_v2 import other_subjects
+import json, datetime, time
+NOW = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+
+def crawlSite(site, YEAR, SEMESTER, CHECKPOINT):
+    log = []
+    lectures = {'checkpoint':0}
+    checkpoint = 0
+
+    print(f'선택한 옵션  : (연도 : {YEAR}, 학기 : {SEMESTER}, ',end='')
+
+    if CHECKPOINT:
+        print(f'체크포인트 : {CHECKPOINT})')
+
+        try:
+            with open(CHECKPOINT, 'r') as f:
+                lectures = json.load(f)
+        except FileNotFoundError:
+            print('오류     : 해당 파일을 찾을 수 없음')
+            exit(0)
+        except:
+            print('오류     : 경로 오류')
+            exit(0)
+        
+        if 'checkpoint' in lectures:
+            checkpoint = lectures['checkpoint']
+        else:
+            print('이미 완성된 파일')
+    else:
+        print(f'체크포인트 : 없음)')
+
+    try:
+        Select(site.find_element(By.ID, 'pYear')).select_by_visible_text(str(YEAR))
+    except NoSuchElementException:
+        print('오류     : 해당 연도를 선택할 수 없음')
+        exit(0)
+    except:
+        print('오류     : 연도 오류')
+        exit(0)
+
+    try:
+        Select(site.find_element(By.ID, 'pTerm')).select_by_index(SEMESTER-1)
+    except NoSuchElementException:
+        print('오류     : 해당 학년를 선택할 수 없음')
+        exit(0)
+    except:
+        print('오류     : 학년 오류')
+        exit(0)
+
+    site.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/form/table/tbody/tr[2]/td[2]/label[2]').click()
+
+    select_class = Select(site.find_element(By.ID, 'pPobt'))
+    select_univ = Select(site.find_element(By.ID, 'pUniv'))
+    select_major = Select(site.find_element(By.ID, 'pSustMjCd'))
+    time.sleep(0.1)
+
+    print('진행과정 : 불러오기 시작')
+    print('----------------------')
+
+    #학기 번호
+    term = {1:'B01011', 2:'B01012', 3:'B01014', 4:'B01015'}
+
+    if checkpoint < 1:
+        print('진행과정 : 전선 시작')
+        major_or_designated(0, site, lectures, select_class, select_univ, select_major, YEAR, term[SEMESTER], log) #전선
+        print('진행과정 : 전선 완료                                     ')
+        lectures['checkpoint'] = 1
+
+        print('전선 저장')
+        with open(f'checkpoint/checkpoint_전선_{NOW}.json', 'w', encoding='UTF-8') as f : 
+            json.dump(lectures, f, indent=4, ensure_ascii=False)
+
+    if checkpoint < 2:
+        print('진행과정 : 전필 시작')
+        major_or_designated(1, site, lectures, select_class, select_univ, select_major, YEAR, term[SEMESTER], log) #전필
+        print('진행과정 : 전필 완료                                     ')
+        lectures['checkpoint'] = 2
+
+        print('전필 저장')
+        with open(f'checkpoint/checkpoint_전필_{NOW}.json', 'w', encoding='UTF-8') as f : 
+            json.dump(lectures, f, indent=4, ensure_ascii=False)
+
+    if checkpoint < 3:
+        print('진행과정 : 지교 시작')
+        major_or_designated(2, site, lectures, select_class, select_univ, select_major, YEAR, term[SEMESTER], log) #지교
+        print('진행과정 : 지교 완료                                     ')
+        lectures['checkpoint'] = 3
+
+        print('지교 저장')
+        with open(f'checkpoint/checkpoint_지교_{NOW}.json', 'w', encoding='UTF-8') as f : 
+            json.dump(lectures, f, indent=4, ensure_ascii=False)
+
+    if checkpoint < 4:
+        print('진행과정 : 지필 시작')
+        major_or_designated(3, site, lectures, select_class, select_univ, select_major, YEAR, term[SEMESTER], log) #지필
+        print('진행과정 : 지필 완료                                     ')
+        lectures['checkpoint'] = 4
+
+        print('지필 저장')
+        with open(f'checkpoint/checkpoint_지필_{NOW}.json', 'w', encoding='UTF-8') as f : 
+            json.dump(lectures, f, indent=4, ensure_ascii=False)
+
+    if checkpoint < 5:
+        print('진행과정 : 일선 시작')
+        other_subjects(4, site, lectures, select_class, YEAR, term[SEMESTER], log)  #일선
+        print('진행과정 : 일선 완료                                     ')
+        print('진행과정 : 교직 시작')
+        other_subjects(5, site, lectures, select_class, YEAR, term[SEMESTER], log)  #교직
+        print('진행과정 : 교직 완료                                     ')
+        lectures['checkpoint'] = 5
+
+        print('일선, 교직 저장')
+        with open(f'checkpoint/checkpoint_일선_교직_{NOW}.json', 'w', encoding='UTF-8') as f : 
+            json.dump(lectures, f, indent=4, ensure_ascii=False)
+
+    if checkpoint < 6:
+        print('진행과정 : 기교 시작')
+        other_subjects(6, site, lectures, select_class, YEAR, term[SEMESTER], log)  #기교
+        print('진행과정 : 기교 완료                                     ')
+        lectures['checkpoint'] = 6
+
+        print('기교 저장')
+        with open(f'checkpoint/checkpoint_일선_교직_{NOW}.json', 'w', encoding='UTF-8') as f : 
+            json.dump(lectures, f, indent=4, ensure_ascii=False)
+    
+    if checkpoint < 7:
+        print('진행과정 : 심교 시작')
+        other_subjects(7, site, lectures, select_class, YEAR, term[SEMESTER], log)  #심교
+        print('진행과정 : 심교 완료                                     ')
+        del lectures['checkpoint']
+
+    # print('진행과정 : 융필 시작')
+    # convergence_required(8, site, lectures)  #융필
+    # print('진행과정 : 융필 완료')
+    # print('진행과정 : 융선 시작')
+    # convergenct_elective(9, site, lectures)  #융선
+    # print('진행과정 : 융선 완료')
+    
+    print('진행과정 : 완료!')
+    with open('dub_log.json', 'w', encoding='UTF-8') as f : 
+	    json.dump(log, f, indent=4, ensure_ascii=False)
+
+    return lectures
+
+
+def readySelenium():
+    options = Options()
+    # gitpod에서 접속하기 위한 옵션
+    options.add_argument("--disable-dev-shm-usage") 
+    options.add_argument("--headless")  
+
+    # Setup ChromeDriver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+
+def makeLectures(YEAR, SEMESTER, CHECKPOINT, URL):
+    site = readySelenium()
+
+    #페이지 접속
+    site.get(URL)
+    print('접속 완료    : ' + site.title)
+
+    lectures = crawlSite(site, YEAR, SEMESTER, CHECKPOINT)
+
+    return lectures
---
+++ subject_crawling_v2.py
@@ -0,0 +1,180 @@
+import time, threading
+from selenium.webdriver.common.by import By
+
+#저장 key들
+#구버전
+# id_dict = {
+#         2: 'course_num', 3: 'class', 4: 'lecnum', 5: 'name', 6: 'credit', 7: 'hour', 8: 'type_name',
+#         9: 'lang', 10: '해설', 11: 'note', 12: 'class_elective', 13: 'grade', 14: 'basic_major', 15: 'instructor', 16: 'info'
+#         }
+
+#신버전 + notice, syllabus_url, commentary_url
+id_dict = {
+        2: 'course_num', 3: 'class', 4: 'lecnum', 5: 'name', 6: 'credit', 7: 'hour', 8: 'type_name',
+        9: 'lang', 10: 'commentary', 11: 'note', 12: 'liberal_arts_area', 13: 'grade', 14: 'basic_major', 15: 'instructor', 16: 'time'
+        }
+
+order = [i for i in range(2, 17)]
+order = [order[2]] + order[:2] + order[3:]    #4->2->3->5->6->...
+
+
+#결과 테이블 순회
+def iterTable(site, lectures, log, year, term, u='', m=''):
+    r = 2
+    moreRow = True
+    
+    #결과 테이블 행 순회
+    while True:
+        #진행 결과(현재 행) 출력
+        print(u,m,str(r),'                            ',end='\r')
+
+        lecnum = 0
+        lecture = {}
+        isExist = False
+
+        #강의계획서 창 전환
+        try:
+            site.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div[3]/div[3]/div/table/tbody/tr[2]/td[4]/button').click()
+            site.switch_to.window(site.window_handles[1])
+
+            #강의계획서 수강신청 유의사항 저장
+            try:
+                lecture['notice'] = site.find_element(By.XPATH, '/html/body/div/div/div[1]/table/tbody/tr[5]/td').text
+            except:
+                print(f"알림     : {u} {m} {r} 수강신청 유의사항 없음      ")
+                lecture['notice'] = ''
+                pass
+            
+            #원래 창으로 전환
+            site.close()
+            site.switch_to.window(site.window_handles[0])
+        except:
+            print(f"알림     : {u} {m} {r} 팝업창 없음      ")
+            lecture['notice'] = ''
+            pass
+
+        for c in order:
+            #해당 요소 불러오기
+            try:
+                i = site.find_element(By.XPATH, f'/html/body/div[2]/div/div/div[2]/div/div[3]/div[3]/div/table/tbody/tr[{r}]/td[{c}]').text
+            except:
+                moreRow = False
+                break
+            
+            if c == 10:
+                continue
+            
+            if c == 4:
+                lecnum = int(site.find_element(By.XPATH, f'/html/body/div[2]/div/div/div[2]/div/div[3]/div[3]/div/table/tbody/tr[{r}]/td[4]/button').text)
+                
+                #중복 확인
+                if lecnum in lectures and 'course_num' in lectures[lecnum]:
+                    log.append((u,m,str(r),str(lecnum)))
+                    isExist = True
+                    break
+                
+                lecture['term'] = term
+                
+                continue
+        
+            lecture[id_dict[c]] = i
+        
+        if not moreRow:
+            break
+        
+        r += 1
+
+        if isExist:
+            continue
+        
+        lectures[lecnum] = lecture.copy()
+        
+
+#전선,전필,지교,지필
+def major_or_designated(idx, site, lectures, select_class, select_univ, select_major, year, term, log):
+    #이수구분 선택
+    select_class.select_by_index(idx)
+    time.sleep(1)
+
+    #대학 필수 알림 뜰 시 확인 클릭
+    try:
+        site.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div/div/div/div/div/div/div[4]/button[1]').click()
+    except:
+        pass
+    
+    #대학들 순회
+    univ = 0
+    
+    while True:
+        #대학 선택
+        try: select_univ.select_by_index(univ)
+        except: break
+
+        #대학 이름 확인
+        univ_element = site.find_element(By.XPATH, f'/html/body/div[2]/div/div/div[1]/form/table/tbody/tr[2]/td[3]/select/option[{univ+1}]')
+        time.sleep(0.5)
+
+        #학과 순회
+        major = 0
+        isVisited = set()
+
+        while True:
+            #학과 선택
+            try: select_major.select_by_index(major)
+            except: break
+
+            #학과 이름 확인
+            major_element = site.find_element(By.XPATH, f'/html/body/div[2]/div/div/div[1]/form/table/tbody/tr[2]/td[4]/select/option[{major+1}]')
+
+            #학과 이미 방문 했는지 확인
+            if major_element in isVisited:
+                major+=1
+                continue
+            else:
+                isVisited.add(major_element)
+            
+            #진행 사항 출력
+            print(univ_element.text,major_element.text,'0                 ',end='\r')
+
+            #검색 버튼 클릭
+            site.find_element(By.ID, 'btnSearch').click()
+            time.sleep(3)
+
+            #검색 결과 테이블 순회
+            iterTable(site, lectures, log, year, term, univ_element.text, major_element.text)
+
+            major+=1
+        univ+=1
+    
+
+#일선, 교직, 기교, 심교, 융필, 융선
+def other_subjects(idx, site, lectures, select_class, year, term, log):
+    #이수 구분 선택
+    select_class.select_by_index(idx)
+    time.sleep(1)
+
+    #검색 버튼 클릭
+    site.find_element(By.ID, 'btnSearch').click()
+    time.sleep(1)
+
+    #검색 결과 테이블 순회
+    iterTable(site, lectures, log, year, term)
+
+class Multithreading(threading.Thread):
+    def __init__(self, idx, site, lectures, select_class, select_univ=None, select_major=None, year=None, term=None, log=None):
+        threading.Thread.__init__(self)
+        self.idx = idx
+        self.site = site
+        self.lectures = lectures
+        self.select_class = select_class
+        self.select_univ = select_univ
+        self.select_major = select_major
+        self.year = year
+        self.term = term
+        self.log = log
+
+    def run(self):
+        if self.select_univ and self.select_major:
+            major_or_designated(self.idx, self.site, self.lectures, self.select_class, self.select_univ, self.select_major, self.year, self.term, self.log)
+        else:
+            other_subjects(self.idx, self.site, self.lectures, self.select_class, self.year, self.term, self.log)
